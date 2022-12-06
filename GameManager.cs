848d1daaff1776f9879c55c5996b946eac1ff445
Kyungmin Seo <71204429+kyungminei@users.noreply.github.com>
2022-12-07 00:15:49 +0900

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,328 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject MenuCamera;
+    public GameObject GameCamera;
+    public Player player;
+    public Boss boss;
+    public GameObject itemShop;
+    public GameObject startZone;
+    public int stage;
+    public float playTime;
+    public bool isBattle;
+    public int enemyCntA;
+    public int enemyCntB;
+    public int enemyCntC;
+    public int enemyCntD;
+    public GameObject[] stageGrounds;
+    public GameObject curGround;
+
+    public enum stageType { spring, summer, fall, winter};
+    public stageType StageType;
+
+
+    public Transform[] enemyZone;
+    public GameObject[] enemies;
+    public List<int> enemyList;
+
+    public GameObject seasonPanel;
+    public GameObject menuPanel;
+    public GameObject gamePanel;
+    public GameObject overPanel;
+
+    public Text maxScoreText;
+
+    public Text scoreText;
+    public Text stageText;
+    public Text playtimeText;
+    public Text playerHealth;
+    public Text playerCoin;
+    public Image weapon1Img;
+    public Text enemyAText;
+    public Text enemyBText;
+    public Text enemyCText;
+    public RectTransform bossHealthGroup;
+    public RectTransform bossHealthBar;
+    public Text curScoreText;
+    public Text bestScoreText;
+    public GameObject stageClearText;
+
+    public AudioClip ClearSound;
+    AudioSource audioSource;
+
+    private void Awake()
+    {
+        enemyList = new List<int>();
+        maxScoreText.text = string.Format("{0:n0}", PlayerPrefs.GetInt("MaxScore"));
+        this.audioSource = GetComponent<AudioSource>();
+
+        if (PlayerPrefs.HasKey("MaxScore"))
+        {
+            PlayerPrefs.SetInt("MaxScore", 0);
+        }
+    }
+
+    public void SetGround(int stage_num, stageType stage_Type)
+    {
+        stage = stage_num;
+        StageType = stage_Type;
+
+        if (stage == 0) return;
+
+        if (stage > 4) Restart();
+
+        switch(StageType)
+        {
+            case stageType.spring:
+                stageGrounds[stage-1].SetActive(true);
+                curGround.SetActive(false);
+                curGround = stageGrounds[stage - 1];
+                break;
+
+            case stageType.summer:
+                stageGrounds[stage - 1].SetActive(true);
+                curGround.SetActive(false);
+                curGround = stageGrounds[stage - 1];
+                break;
+
+            case stageType.fall:
+                stageGrounds[stage - 1].SetActive(true);
+                curGround.SetActive(false);
+                curGround = stageGrounds[stage - 1];
+                break;
+
+            case stageType.winter:
+                stageGrounds[stage - 1].SetActive(true);
+                curGround.SetActive(false);
+                curGround = stageGrounds[stage - 1];
+                break;
+        }
+
+        GameStart();
+    }
+
+    public void GameStart()
+    {
+        MenuCamera.SetActive(false);
+        GameCamera.SetActive(true);
+
+        //menuPanel.SetActive(false);
+        seasonPanel.SetActive(false);
+
+        gamePanel.SetActive(true);
+
+        player.gameObject.SetActive(true);
+    }
+
+    public void GameOver()
+    {
+        gamePanel.SetActive(false);
+        overPanel.SetActive(true);
+        curScoreText.text = scoreText.text;
+
+        int maxScore = PlayerPrefs.GetInt("MaxScore");
+        if(player.score>maxScore)
+        {
+            bestScoreText.gameObject.SetActive(true);
+            PlayerPrefs.GetInt("MaxScore", player.score);
+        }
+    }
+
+    public void OnSeasonPanel()
+    {
+        menuPanel.SetActive(false);
+        seasonPanel.SetActive(true);
+    }
+
+    public void QuitGameButton()
+    {
+        Application.Quit();
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void StageStart()
+    {
+        itemShop.SetActive(false);
+        //weaponShop.SetActive(false);
+        startZone.SetActive(false);
+
+        foreach( Transform zone in enemyZone)
+        {
+            zone.gameObject.SetActive(true);
+        }
+
+        isBattle = true;
+        StartCoroutine(InBattle());
+    }
+
+    public void StageEnd()
+    {
+        player.transform.position = Vector3.up * 0.8f;
+        player.isInSlowZone = false;
+        player.speed = player.standardSpeed;
+
+        itemShop.SetActive(true);
+        //weaponShop.SetActive(true);
+        startZone.SetActive(true);
+
+        foreach (Transform zone in enemyZone)
+        {
+            zone.gameObject.SetActive(false);
+        }
+
+        isBattle = false;
+        stage++;
+        SetGround(stage, StageType);
+    }
+
+    IEnumerator InBattle()
+    {
+        if(stage==4)
+        {
+            enemyCntD++;
+            GameObject instantEnemy = Instantiate(enemies[3], enemyZone[0].position, enemyZone[0].rotation);
+            Enemy enemy = instantEnemy.GetComponent<Enemy>();
+            enemy.Target = player.transform;
+            enemy.manager = this;
+            boss = instantEnemy.GetComponent<Boss>();
+        }
+        else
+        {
+            int monsterCount = 0;
+
+            for (int index = 0; index < (30 + stage * 5); index++)
+            {
+                int ran = Random.Range(0, 11);
+                enemyList.Add(monsterCount);
+
+                switch (ran)
+                {
+                    case 0:
+                        enemyCntA++;
+                        monsterCount = 0;
+                        break;
+                    case 1:
+                        enemyCntA++;
+                        monsterCount = 0;
+                        break;
+                    case 2:
+                        enemyCntA++;
+                        monsterCount = 0;
+                        break;
+                    case 3:
+                        enemyCntA++;
+                        monsterCount = 0;
+                        break;
+                    case 4:
+                        enemyCntA++;
+                        monsterCount = 0;
+                        break;
+                    case 5:
+                        enemyCntA++;
+                        monsterCount = 0;
+                        break;
+                    case 6:
+                        enemyCntA++;
+                        monsterCount = 0;
+                        break;
+                    case 7:
+                        enemyCntA++;
+                        monsterCount = 0;
+                        break;
+                    case 8:
+                        enemyCntA++;
+                        monsterCount = 0;
+                        break;
+                    case 9:
+                        enemyCntB++;
+                        monsterCount = 1;
+                        break;
+                    case 10:
+                        enemyCntC++;
+                        monsterCount = 2;
+                        break;
+                }
+            }
+        }
+
+        while(enemyList.Count > 0)
+        {
+            int ran = Random.Range(0, 8);
+            GameObject instantEnemy = Instantiate(enemies[enemyList[0]], enemyZone[ran].position, enemyZone[ran].rotation);
+            Enemy enemy = instantEnemy.GetComponent<Enemy>();
+            enemy.Target = player.transform;
+            enemy.manager = this;
+            enemyList.RemoveAt(0);
+            yield return new WaitForSeconds(0.4f); //Enumerator의 while문 안에 yield return을 포함시키는 것이 좋다.
+
+        }
+
+        while(enemyCntA+enemyCntB+enemyCntC+enemyCntD>0)
+        {
+            yield return null;
+        }
+
+        stageClearText.SetActive(true);
+        audioSource.clip = ClearSound;
+        audioSource.Play();
+        yield return new WaitForSeconds(4f);
+
+        boss = null;
+        stageClearText.SetActive(false);
+        StageEnd();
+    }
+
+    private void Update()
+    {
+        if(isBattle)
+        {
+            playTime += Time.deltaTime;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        //상단 UI
+        scoreText.text= string.Format("{0:n0}", player.score);
+        stageText.text = "STAGE " + stage;
+
+        int hour = (int)(playTime / 3600);
+        int min = (int)((playTime - hour * 3600) / 60);
+        int second = (int)(playTime % 60);
+
+        playtimeText.text = string.Format("{0:00}", hour) + ":" + string.Format("{0:00}", min) + ":" + string.Format("{0:00}", second);
+
+        //플레이어 UI
+        playerHealth.text = player.health + "/" + player.maxhealth;
+        playerCoin.text= string.Format("{0:n0}", player.coin);
+
+        //무기 UI
+        //weapon1Img.color = new Color(1, 1, 1, player.hasweapon ? 1 : 0);
+
+        //몬스터 숫자UI
+        enemyAText.text = enemyCntA.ToString();
+        enemyBText.text = enemyCntB.ToString();
+        enemyCText.text = enemyCntC.ToString();
+
+        //보스 체력 UI
+        if(boss!=null)
+        {
+            bossHealthGroup.anchoredPosition = Vector3.down* 30;
+            bossHealthBar.localScale = new Vector3((float)boss.curHealth / boss.maxHealth, 1, 1);
+        }
+        else
+        {
+            bossHealthGroup.anchoredPosition = Vector3.up * 200;
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,505 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.AI;
+using static UnityEngine.GraphicsBuffer;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public float slowSpeed;
+    public float standardSpeed; //하이어라키창에서 수정안해도 됨.
+    public GameObject weapon;
+    public Camera followCamera;
+    public bool hasweapon;
+    public GameManager manager;
+    public GameObject ChargeEffect;
+    public GameObject arrow;
+
+    public AudioClip wAttackSound;
+    public AudioClip cAttackSound;
+    public AudioClip DodgeSound;
+    public AudioClip CoinSound;
+    AudioSource audioSource;
+
+    public int coin;
+    public int health;
+    public int score;
+
+    public int maxcoin;
+    public int maxhealth;
+
+    float hAxis;
+    float vAxis;
+
+    float curChargeTime=0;
+
+    bool iDown; //weapon
+    bool wDown;
+    bool fDown;
+    bool fUp;
+    bool gDown;
+    bool rDown;
+    bool jDown;
+    bool sDown1;
+    bool sDown2;
+    bool sDown3;
+
+
+    bool isJump;
+    bool isDodge;
+    bool isFireReady = true;
+    bool isBorder;
+    bool isDamaged;
+    bool isShop;
+    bool isDead;
+    bool isClicked;
+    bool isCharge;
+    bool isAttackTurn; // 공격에 의한 회전을 하는 중인지 아닌지.
+    public bool isInSlowZone;
+
+    Vector3 movevec;
+    Vector3 dodgevec;
+    Vector3 mousevec;
+
+    Animator animator;
+    Rigidbody rigid;
+    //MeshRenderer[] meshs;
+    SkinnedMeshRenderer skMat;
+    Color firstColor; //처음 캐릭터 색상 저장
+
+    GameObject nearobject;
+    public Weapon equipWeapon; //장착중인 무기
+    int equipWeaponIndex= -1;
+    float fireDelay;
+
+    private void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+        rigid = GetComponent<Rigidbody>();
+        //meshs= GetComponentsInChildren<MeshRenderer>();
+        skMat = GetComponentInChildren<SkinnedMeshRenderer>();
+
+        PlayerPrefs.SetInt("MaxScore",0);
+        Debug.Log(PlayerPrefs.GetInt("MaxScore"));
+
+        firstColor = skMat.materials[0].color;
+        standardSpeed = speed;
+
+        this.audioSource = GetComponent<AudioSource>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        GetInput();
+        Move();
+        Turn();
+        arrowTurn();
+        RecordFDownTime();
+        Attack();
+        Dodge();
+        interation();
+    }
+
+    void GetInput()
+    {
+        hAxis = Input.GetAxisRaw("Horizontal"); //GetAxisRaw : -1, 0 ,1 
+        vAxis = Input.GetAxisRaw("Vertical");
+        wDown = Input.GetButton("Walk"); //수정 필
+        jDown = Input.GetButtonDown("Jump");
+        fDown = Input.GetButtonDown("Fire1");
+        fUp = Input.GetButtonUp("Fire1");
+        iDown = Input.GetButtonDown("interation");
+        sDown1 = Input.GetButtonDown("swap1");
+        sDown2 = Input.GetButtonDown("swap2");
+        sDown3 = Input.GetButtonDown("swap3");
+    }
+
+    void PlaySound(string action)
+    {
+        switch (action)
+        {
+            case "wAttack":
+                audioSource.clip = wAttackSound;
+                break;
+            case "cAttack":
+                audioSource.clip = cAttackSound;
+                break;
+            case "Dodge":
+                audioSource.clip = DodgeSound;
+                break;
+
+            case "Coin":
+                audioSource.clip = CoinSound;
+                break;
+        }
+        audioSource.Play();
+    }
+
+    void Move()
+    {
+        movevec = new Vector3(hAxis, 0, vAxis).normalized; 
+
+        if(isDodge) 
+        {
+            movevec = dodgevec;
+        }
+
+        if( isDead) //!isFireReady뺌
+        {
+            movevec = Vector3.zero;
+        }
+
+        if(!isBorder)
+        {
+            transform.position += movevec * speed  * Time.deltaTime;
+        }
+
+        animator.SetBool("Iswalk", movevec != Vector3.zero);
+        //animator.SetBool("Iswalk", wDown);
+    }
+
+    void Turn()
+    {
+        if(!isAttackTurn)
+        {
+            //키보드 회전
+            transform.LookAt(transform.position + movevec);
+
+            //마우스 회전
+            if (fDown && !isDead)
+            {
+                Ray ray = followCamera.ScreenPointToRay(Input.mousePosition);
+                RaycastHit rayHit; //ray에 닿은 오브젝트 정보를 저장하는 변수
+                if (Physics.Raycast(ray, out rayHit, 100))
+                {
+                    Vector3 nextVec = rayHit.point - transform.position;
+                    nextVec.y = 0;
+                    transform.LookAt(transform.position + nextVec);
+                }
+            }
+        }
+    }
+
+    void arrowTurn()
+    {
+        Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane GroupPlane = new Plane(Vector3.up, Vector3.zero);
+
+        float rayLength;
+
+        if (GroupPlane.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 pointTolook = cameraRay.GetPoint(rayLength);
+            arrow.transform.LookAt(new Vector3(pointTolook.x, transform.position.y, pointTolook.z));
+        }
+    }
+
+
+    void Jump()
+    {
+        if(jDown && movevec==Vector3.zero && !isJump && !isDodge && !isDead)
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * 20, ForceMode.Impulse);
+            animator.SetBool("Isjump", true);
+            animator.SetTrigger("Dojump");
+            isJump = true;
+        }
+    }
+
+    void Dodge()
+    {
+        if (jDown && movevec != Vector3.zero && !isJump && !isDodge && !isDead ) 
+        {
+            dodgevec = movevec;
+            PlaySound("Dodge");
+            animator.SetTrigger("Dododge");
+            speed *= 2;
+            isDodge = true;
+            Debug.Log(speed);
+
+            Invoke("DodgeOut", 0.7f);
+        }
+    }
+
+    void DodgeOut()
+    {
+        if(isInSlowZone)
+        {
+            speed = slowSpeed;
+            isDodge = false;
+            return;
+        }
+        else
+        {
+            speed = standardSpeed;
+            isDodge = false;
+        }
+        //speed *= 0.5f;
+    }
+
+    void interation()
+    {
+        if (iDown && nearobject != null && !isJump && !isDodge && !isDead)
+        {
+            if (nearobject.tag == "Weapon")
+            {
+                Item item = nearobject.GetComponent<Item>();
+                int weaponIndex = item.value;
+
+                hasweapon = true;
+
+                Destroy(nearobject);
+            }
+            else if (nearobject.tag == "Shop")
+            {
+                Shop shop = nearobject.GetComponent<Shop>();
+                shop.Enter(this);
+                isShop = true;
+            }
+        }
+    }
+
+    void Attack()
+    {
+        if (equipWeapon == null) return;
+
+        fireDelay += Time.deltaTime;
+        isFireReady = equipWeapon.rate < fireDelay;
+
+        if (isCharge && isFireReady && !isDodge && !isShop && !isDead && !isClicked)
+        {
+            StartCoroutine("SeeToMousePos");
+
+            equipWeapon.ChargeAttack();
+            animator.SetTrigger("Doattack");
+
+            PlaySound("cAttack");
+            fireDelay = 0;
+            isCharge = false;
+        }
+        else if (fUp && isFireReady && !isDodge && !isShop && !isDead && !isCharge)
+        {
+            StopCoroutine("SeeToMousePos");
+            StartCoroutine("SeeToMousePos");
+
+            equipWeapon.MeleeAttack();
+            animator.SetTrigger("Doattack");
+
+            PlaySound("wAttack");
+            fireDelay = 0;
+        }
+    }
+
+    void SaveMousePos()
+    {    
+        Ray ray = followCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit rayHit; //ray에 닿은 오브젝트 정보를 저장하는 변수
+        if (Physics.Raycast(ray, out rayHit, 100))
+        {
+            mousevec = rayHit.point; //마우스 누른 위치 저장
+        }    
+    }
+
+    IEnumerator SeeToMousePos()
+    {
+        Vector3 tomouse = mousevec - transform.position;
+        tomouse.y = 0;
+        transform.LookAt(transform.position+tomouse);
+
+        isAttackTurn = true;
+        yield return new WaitForSeconds(0.5f);
+
+        isAttackTurn = false;
+    }
+   
+    void RecordFDownTime() //공격버튼 누른 시간 기록
+    {
+        if (fDown)
+        {
+            isClicked = true;
+        }
+        if (fUp)
+        {   
+            ChargeEffect.SetActive(false);
+            isClicked = false;
+            curChargeTime = 0;
+        }
+
+        if (isClicked)
+        {
+            SaveMousePos();
+            ChargeEffect.SetActive(true);
+            curChargeTime += Time.deltaTime;
+
+            if (curChargeTime >= equipWeapon.maxChargeTime)
+            {
+                isCharge = true;
+            }
+        }
+    }
+
+    void FreezeRotation()
+    {
+        rigid.angularVelocity = Vector3.zero;
+    }
+
+    void StopToWall()
+    {
+        Debug.DrawRay(transform.position, transform.forward * 5, Color.blue);
+        isBorder = Physics.Raycast(transform.position, transform.forward, 5, LayerMask.GetMask("Wall"));
+    }
+     void FixedUpdate()
+    {
+        FreezeRotation();
+        StopToWall();
+    }
+
+    /*private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag=="Floor")
+        {
+            animator.SetBool("Isjump", false);
+            isJump = false;
+        }
+    }*/ //점프모션 추가되면 활성화
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Item")
+        {
+            Item item = other.GetComponent<Item>();
+            switch (item.type)
+            {
+                case Item.Type.Coin:
+                    coin += item.value;
+                    //PlaySound("Coin");
+                    if (coin > maxcoin)
+                    {
+                        coin = maxcoin;
+                    }
+                    break;
+                case Item.Type.Heart:
+                    health += item.value;
+                    if (health > maxhealth)
+                    {
+                        health = maxhealth;
+                    }
+                    break;
+            }
+            Destroy(other.gameObject);
+        }
+        else if (other.tag == "EnemyBullet")
+        {
+            if (!isDamaged)
+            {
+                Bullet enemyBullet = other.GetComponent<Bullet>();
+                health -= enemyBullet.damage;
+
+                bool isBossAttack = other.name == "Boss Melee Area";
+
+                StartCoroutine(OnDamage(isBossAttack));
+            }
+
+            if (other.GetComponent<Rigidbody>() != null)
+                Destroy(other.gameObject);
+        }
+        else if(other.tag=="FallingObject")
+        {
+            if(!isDamaged)
+            {
+                FallingObject fallingObj = other.GetComponent<FallingObject>();
+                health -= fallingObj.Damage;
+                fallingObj.particle.gameObject.SetActive(true);
+
+                Vector3 reactVec = gameObject.transform.position - other.transform.position;
+                reactVec = reactVec.normalized;
+                rigid.AddForce(reactVec * 5,ForceMode.Impulse);
+
+                StartCoroutine(OnDamage(false));
+
+                Destroy(other.gameObject);
+            }
+        }
+        else if (other.tag == "SlowZone")
+        {
+            isInSlowZone = true;
+            speed = slowSpeed;
+        }
+    }
+
+    IEnumerator OnDamage(bool isBossAttack)
+    {
+        isDamaged = true;
+
+        skMat.materials[0].color = Color.yellow;
+
+        if(isBossAttack)
+        {
+            rigid.AddForce(transform.forward * -25, ForceMode.Impulse);
+        }
+
+        if (health <= 0 && !isDead)
+        {
+            OnDie();
+        }
+
+        yield return new WaitForSeconds(1f);
+
+        isDamaged = false;
+
+        skMat.materials[0].color = firstColor;
+
+        if (isBossAttack)
+        {
+            rigid.velocity = Vector3.zero;
+        }
+
+    }
+
+    void OnDie()
+    {
+        animator.SetTrigger("Dodie");
+        isDead = true;
+        manager.GameOver();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag=="Weapon" || other.tag == "Shop")
+        {
+            nearobject = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        //if(other.tag == "Weapon")
+        //{
+        //    nearobject = null;
+        //}
+        //위 코드 때문에, weapon Shop에서 무기 구매 후 무기가 가까이에 있으면 null처리되어 아래문장에 에러가 생김.
+        //(nearobject가 null이 되어버려서 상점에서 나와지지 않음)
+        //위 코드가 필요해질 시, 주석을 해제하고, weapon Shop의 weapon spawn pos을 멀리 떨어진 곳으로 옮기기.
+        if (other.tag == "Shop")
+        {
+            Shop shop = nearobject.GetComponent<Shop>();
+            shop.Exit();
+            isShop = false;
+
+            nearobject = null;
+        }
+        else if(other.tag=="SlowZone")
+        {
+            isInSlowZone = false;
+            speed = standardSpeed;
+        }
+    }
+}
